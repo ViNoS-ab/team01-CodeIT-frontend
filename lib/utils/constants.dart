8e66d4cf13d71ef97cc75f92d3a377b53fc7f033
oihid <c.ouchene@esi-sba.dz>
2023-02-06 15:49:43 +0100

    added assets+My tasks screen

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,3 +1,10 @@
 // we can put our constants like colors and strings here.
+import 'package:flutter/material.dart';
+const Klightred=Color(0xFFF1867C);
+const kGreen=Color(0xFF0D9D58);
+final Kyellow=Color(0xFFFDC72E).withOpacity(0.8);
+final KlightBlue=Color(0xFF7BA8F3).withOpacity(0.8);
+
+
 
 double borderRadius = 20;
